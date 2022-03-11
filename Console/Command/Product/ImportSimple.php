8fa6e1a65c2d89ea7d9ad36b4bbce1f9255a03ab
Andreas von Studnitz <avs@integer-net.de>
2022-03-11 13:34:09 +0100

    Optimize sample import file

--- Console/Command/Product/ImportSimple.php
+++ Console/Command/Product/ImportSimple.php
@@ -1,73 +1,119 @@
 <?php
 /**
- * Copyright © 2016 FireGento e.V. - All rights reserved.
+ * Copyright © 2016 - 2022 FireGento e.V. - All rights reserved.
  * See LICENSE.md bundled with this module for license details.
  */
+
 namespace FireGento\FastSimpleImportDemo\Console\Command\Product;
-use FireGento\FastSimpleImportDemo\Console\Command\AbstractImportCommand;
-use Magento\Framework\App\ObjectManagerFactory;
-use Magento\ImportExport\Model\Import;
+
+use FireGento\FastSimpleImport\Model\Importer;
 use Magento\Catalog\Api\ProductAttributeOptionManagementInterface;
+use Magento\Catalog\Model\Product;
+use Magento\ImportExport\Model\Import;
+use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingErrorAggregatorInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 /**
- * Class TestCommand
+ * Class ImportSimple
+ *
  * @package FireGento\FastSimpleImport2\Console\Command
  *
  */
-class ImportSimple extends AbstractImportCommand
+class ImportSimple extends Command
 {
-    /**
-     * @var ProductAttributeOptionManagementInterface
-     */
-    private $attributeOptionManagementInterface;
+    private Importer                                  $importer;
+    private ProductAttributeOptionManagementInterface $productAttributeOptionManagement;
 
+    public function __construct(
+        Importer $importer,
+        ProductAttributeOptionManagementInterface $productAttributeOptionManagement,
+        string $name = null
+    ) {
+        parent::__construct($name);
+        $this->importer = $importer;
+        $this->productAttributeOptionManagement = $productAttributeOptionManagement;
+    }
 
-    public function deleteOption($attrCode,$label){
-        $attributeOptionList = $this->attributeOptionManagementInterface->getItems($attrCode);
+    public function deleteOption(string $attributeCode, string $label)
+    {
+        $attributeOptionList = $this->productAttributeOptionManagement->getItems($attributeCode);
         foreach ($attributeOptionList as $attributeOptionInterface) {
             if ($attributeOptionInterface->getLabel() == $label) {
-               $this->attributeOptionManagementInterface->delete($attrCode,$attributeOptionInterface->getValue() );
+                $this->productAttributeOptionManagement->delete($attributeCode, $attributeOptionInterface->getValue());
             }
         }
     }
 
-
     protected function configure()
     {
-        $this->setName('fastsimpleimportdemo:products:importsimple')
+        $this
+            ->setName('fastsimpleimportdemo:products:importsimple')
             ->setDescription('Import Simple Products ');
-
-        $this->setBehavior(Import::BEHAVIOR_ADD_UPDATE);
-        $this->setEntityCode('catalog_product');
 
         parent::configure();
     }
 
     /**
-     * @return array
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return null|int null or 0 if everything went fine, or an error code
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
-    protected function getEntities()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->attributeOptionManagementInterface = $this->objectManager->get("Magento\Catalog\Api\ProductAttributeOptionManagementInterface");
-        $this->deleteOption('color', 'PurpleBlue' );
+        $output->writeln('Import started');
+
+        $time = microtime(true);
+
+        $importer = $this->importer;
+
+        // The following settings are optional as they use the default settings:
+        $importer->setBehavior(Import::BEHAVIOR_REPLACE);
+        $importer->setEntityCode(Product::ENTITY);
+        $importer->setValidationStrategy(ProcessingErrorAggregatorInterface::VALIDATION_STRATEGY_STOP_ON_ERROR);
+        $importer->setAllowedErrorCount(0);
+        $importer->setIgnoreDuplicates(false);
+
+        try {
+            $importer->processImport($this->getImportData());
+            $output->write($importer->getErrorMessages()); // displays non-fatal errors if errors are skipped
+        } catch (\Exception $e) {
+            $output->writeln($e->getMessage()); // displays error messages if import failed
+            return 1;
+        }
+
+        $output->writeln('Import finished. Elapsed time: ' . round(microtime(true) - $time, 2) . 's' . "\n");
+
+        return 0;
+    }
+
+    private function getImportData(): array
+    {
+        $this->deleteOption('color', 'PurpleBlue');
         $data = [];
-        for ($i = 1; $i <= 1; $i++) {
-            $data[] = array(
-                'sku' => 'FIREGENTO-' . $i,
+        for ($i = 1; $i <= 10; $i++) {
+            $data[] = [
+                'sku'                => 'FIREGENTO-' . $i,
                 'attribute_set_code' => 'Default',
-                'product_type' => 'simple',
-                'product_websites' => 'base',
-                'name' => 'FireGento Test Product ' . $i,
-                'price' => '14.0000',
-                'ean' => "1234",
-                'color' => "PurpleBlue"
+                'product_type'       => 'simple',
+                'product_websites'   => 'base',
+                'name'               => 'FireGento Test Product ' . $i,
+                'url_key'            => 'firegento-test-product-' . $i,
+                'price'              => '14.0000',
+                'stock_status'       => '1',
+                'ean'                => '1234',
+                'color'              => 'PurpleBlue',
                 //'visibility' => 'Catalog, Search',
                 //'tax_class_name' => 'Taxable Goods',
                 //'product_online' => '1',
                 //'weight' => '1.0000',
                 //'short_description' => NULL,
                 //'description' => '',
-            );
+            ];
         }
+
         return $data;
     }
 }
